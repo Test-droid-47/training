f7de148407836f93324a6a7a041a59372aab473f
Test-droid-47 <subhankhanofficial47@gmail.com>
2026-07-01 22:37:25 +0500

    Fix loss function name for direction prediction

--- prediction_model.py
+++ prediction_model.py
@@ -112,7 +112,7 @@ class PredictionModel:
             optimizer=AdamW(learning_rate=lr_schedule, weight_decay=1e-4),
             loss={
                 'price_pred': 'huber',
-                'direction': 'sparse_Categorical_Crossentropy',
+                'direction': 'sparse_categorical_crossentropy',
                 'entry_quality': 'huber',
                 'exit_bar': 'huber',
                 'position_size': 'huber'
